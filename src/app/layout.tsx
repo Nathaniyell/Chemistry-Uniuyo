fb176c73e0e7bde92e5f9d463ce2bf7a4d331240
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-05-01 09:54:51 +0100

    modified layout.tsx added opengraph-image.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,13 @@ import "../styles/globals.css";
 export const metadata: Metadata = {
   title: "Welcome to the Department of Chemistry, University of Uyo",
   description: "Browse through our amazing variety of courses",
+  authors: [
+    { name: "Nathaniel Essien" },
+    {
+      name: "Clever Akanimoh",
+    },
+  ],
+  metadataBase: new URL("https://chemistry-uniuyo.vercel.sh"),
 };
 
 export default function RootLayout({
@@ -15,10 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en"
-      className={`${lusitana.variable} font-serif`}
-    >
+    <html lang="en" className={`${lusitana.variable} font-serif`}>
       <Providers>
         <body className="scroll-smooth w-full overflow-x-hidden">
           <NavBar />
