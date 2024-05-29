5087574ec88db69a20fa8d9d58474c8ff72a5f28
nathaniyelll <ubong8960@gmail.com>
2024-05-29 08:05:24 +0100

    update HTMLTags.tsx

--- src/components/HTMLTags.tsx
+++ src/components/HTMLTags.tsx
@@ -63,7 +63,7 @@ export const Button = ({
       className={`flex group justify-center items-center min-w-10 min-h-8 transition-all ease-in-out duration-200 text-center text-base xs:text-sm ${className} bg-${variant} border hover:shadow px-6 py-2`}
     >
       {children}{" "}
-      {arrow && <MdOutlineKeyboardDoubleArrowRight className="mt-1 text-2xl xs:text-xl opacity-0 translate-x-[500%] transition-all ease-linear duration-200 group-hover:translate-x-[20%] group-hover:opacity-100" />}
+      {arrow && <MdOutlineKeyboardDoubleArrowRight className="text-2xl xs:text-xl opacity-0 translate-x-[500%] transition-all ease-linear duration-200 group-hover:translate-x-[20%] group-hover:opacity-100" />}
     </m.button>
   );
 };
