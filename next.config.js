29fff95cc2e054341cf0694ea03af891919a92f9
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-05-31 02:51:00 +0100

    modified next.config.js and resources.ts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "https://chemistryuniuyo.vercel.app/",
+      "https://chemuniuyo.vercel.app/",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/lib/resources.ts
+++ src/lib/resources.ts
@@ -1,5 +1,4 @@
 export const allResources = [
-  // lab scene
   {
     heading: "Lab scenery",
     description: "",
