e50092450c3176b9355e4422d87c14cd25f81cd4
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-06-06 10:20:45 +0100

    modified page.tsx in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,14 @@
 "use client";
 import React from "react";
-import {
-  Autoplay,
-  Pagination,
-  // Navigation,
-  // EffectCreative,
-  // EffectCards,
-  EffectFade,
-} from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/css";
-import "swiper/css/effect-flip";
-import "swiper/css/effect-fade";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/navigation";
+
 import Image from "next/image";
 import eduInam from "../../public/images/teachingstaff/eduinam4.jpg";
 import profInam from "../../public/images/teachingstaff/profInam2.jpg";
-import homeData, { studentsData, homeStaffData } from "@/lib/data";
+import homeData, { homeStaffData } from "@/lib/data";
 import { Card, HomepageSlider } from "@/components";
 import HomeSections from "@/components/HomeSections";
 import Lecturers from "@/components/Lecturers";
@@ -29,14 +19,26 @@ import { publications } from "@/lib";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
 import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 
+import { useMediaQuery } from "react-responsive";
+
 export default function Home() {
   const headOfDepartment = [
     { title: "prof1", src: eduInam },
     { title: "prof2", src: profInam },
   ];
 
-  const btnClasses = "rounded-full bg-primary hover:border-primary text-white"
+  const btnClasses = "rounded-full bg-primary hover:border-primary text-white";
   const swiper = useSwiper();
+  const isBigMobile = useMediaQuery({ query: "(min-width: 640px)" });
+
+  const isTablet = useMediaQuery({
+    query: "(min-width: 768px)",
+  });
+
+  const isLaptop = useMediaQuery({
+    query: "(min-width: 1024px)",
+  });
+
   return (
     <main className="w-full min-h-screen relative bg-slate-100">
       <Swiper
@@ -199,53 +201,62 @@ export default function Home() {
         />
       </section> */}
       <div className="bg-white mt-10">
-
         <section className="flex flex-col p-4 gap-14 w-11/12 items-center mx-auto">
           <div className="grid place-items-center grid-cols-1 gap-10">
-            <h1 className="text-center text-primary px-4 mx-auto border-r-secondary border-r-4 uppercase font-black text-[24px] md:text-[30px]">Recent News</h1>
+            <h1 className="text-center text-primary px-4 mx-auto border-r-secondary border-r-4 uppercase font-black text-[24px] md:text-[30px]">
+              Recent News
+            </h1>
             <News />
           </div>
         </section>
         <div className="bg-orange-100 my-10 p-20 flex flex-col gap-10 md:flex-row justify-between items-center">
-          <h1 className="font-black text-amber-800 w-full md:w-1/2 lg:w-2/5 text-2xl">Explore the Impressive Resources and Facilities of the Department of Chemistry, University of Uyo</h1>
+          <h1 className="font-black text-amber-800 w-full md:w-1/2 lg:w-2/5 text-2xl">
+            Explore the Impressive Resources and Facilities of the Department of
+            Chemistry, University of Uyo
+          </h1>
 
           <Button
             variant="transparent"
             className="text-white !bg-amber-900 shadow-md border-none !text-2xl !text-center"
             arrow
           >
-            <Link href="/resources">
-              Take tour
-            </Link>
+            <Link href="/resources">Take tour</Link>
           </Button>
         </div>
         <section className="flex flex-col p-4 gap-14 w-11/12 items-center mx-auto">
           <div className="space-y-10 w-full">
-            <h1 className="text-center w-full text-primary px-4 mx-auto border-l-secondary border-l-4 uppercase font-black text-[24px] md:text-[30px]">Recent Research Expeditions</h1>
+            <h1 className="text-center w-full text-primary px-4 mx-auto border-l-secondary border-l-4 uppercase font-black text-[24px] md:text-[30px]">
+              Recent Research Expeditions
+            </h1>
 
             {/* break into a component later */}
             <aside>
               <Swiper className="w-[98%] p-2">
-
                 {publications
                   .slice(0, 4)
-                  .map(({ title, unit, author, published_at, desc, href }, index) => (
-                    <SwiperSlide key={index}>
-                      <Card
-                        isResearchPage={false}
-                        title={title}
-                        type="publication"
-                        unit={unit ?? ""}
-                        author={author}
-                        published_at={published_at}
-                        desc={desc}
-                        href={href}
-                      /></SwiperSlide>
-                  ))}
+                  .map(
+                    (
+                      { title, unit, author, published_at, desc, href },
+                      index
+                    ) => (
+                      <SwiperSlide key={index}>
+                        <Card
+                          isResearchPage={false}
+                          title={title}
+                          type="publication"
+                          unit={unit ?? ""}
+                          author={author}
+                          published_at={published_at}
+                          desc={desc}
+                          href={href}
+                        />
+                      </SwiperSlide>
+                    )
+                  )}
 
                 <div className="flex gap-2 justify-center mt-4">
-                  <Button className={btnClasses} onClick={() => swiper.slidePrev()}><ArrowLeftIcon className="w-4" /></Button>
-                  <Button className={btnClasses} onClick={() => swiper.slideNext()}><ArrowRightIcon className="w-4" /></Button>
+                  <SlideButton Icon={ArrowLeftIcon} />
+                  <SlideButton Icon={ArrowRightIcon} />
                 </div>
               </Swiper>
             </aside>
@@ -260,5 +271,24 @@ export default function Home() {
         </section>
       </div>
     </main>
+  );
+}
+
+function SlideButton({
+  position = "right",
+  Icon,
+}: {
+  position?: "right" | "left";
+  Icon: React.ElementType;
+}) {
+  const { slideNext, slidePrev } = useSwiper();
+
+  return (
+    <button
+      onClick={() => (position === "right" ? slideNext() : slidePrev())}
+      className="bg-transparent p-2 rounded-full text-primary hover:bg-primary border hover:text-white border-primary active:scale-105 transition-all duration-300"
+    >
+      <Icon className="size-7 xs:size-4 " />
+    </button>
   );
 }
