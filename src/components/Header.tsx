d50331604c0874e82e53344645a4a51d5fc9871f
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-06-07 14:15:31 +0100

    modified page.tsx and Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,14 +14,14 @@ type HeaderProps = {
 
 export default function Header({ headerBg, heading, tagline }: HeaderProps) {
   return (
-    <header className="relative min-h-[30vh] w-full">
+    <header className="relative min-h-[20vh] w-full">
       <Image
         src={headerBg}
         alt={"header background"}
         className="absolute top-0 left-0 -z-10 size-full opacity-90"
       />
 
-      <div className="flex flex-col items-center justify-center gap-3 bg-black/60 text-white min-h-[35vh] xs:min-h-[50vh] md:min-h-[70vh] p-4 z-10">
+      <div className="flex flex-col items-center justify-center gap-3 bg-black/65 text-white min-h-[40vh] xs:min-h-[50vh] md:min-h-[70vh] p-4 z-10">
         <motion.h1
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
