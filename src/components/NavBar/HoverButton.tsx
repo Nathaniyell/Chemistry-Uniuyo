a96dfc950b95b2fb99bafd2c12c7768dfaea06e8
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-06-07 04:28:02 +0100

    modified HoverButton.tsx and NavLink.tsx in NavBar

--- src/components/NavBar/HoverButton.tsx
+++ src/components/NavBar/HoverButton.tsx
@@ -27,11 +27,11 @@ export default function HoverButton({
         } capitalize flex items-center gap-2 group-hover:font-bold p-1 md:p-0 transition-colors duration-500`}
       >
         {icon1}
-        <span>{heading}</span>
-        <BsChevronDown className="group-hover:rotate-180 transition-all duration-500" />
+        <span className="md:text-sm">{heading}</span>
+        <BsChevronDown className="group-hover:rotate-180 transition-all duration-500 size-3" />
       </button>
 
-      <div className="h-0 overflow-hidden opacity-0 group-hover:opacity-100 flex flex-col gap-1 md:gap-3  group-hover:h-full bg-blue-50 md:group-hover:h-fit md:min-w-[145px] md:max-w-xs md:fixed md:top-[71px] lg:top-[91px] rounded-md md:bg-white md:border md:border-blue-100 md:ml-0 md:p-4 pt-1 ml-6 transition-all duration-500">
+      <div className="h-0 overflow-hidden opacity-0 group-hover:opacity-100 flex flex-col gap-1 md:text-sm md:gap-3  group-hover:h-full bg-blue-50 md:group-hover:h-fit min-w-[145px] md:max-w-xs md:fixed md:top-[71px] lg:top-[91px] rounded-md md:bg-white md:border md:border-blue-100 md:ml-0 md:p-4 pt-1 ml-6 transition-all duration-500">
         {linksArray.map(({ href, title }, i) => (
           <NavLink key={i} href={href}>
             {title}
--- src/components/NavBar/NavLink.tsx
+++ src/components/NavBar/NavLink.tsx
@@ -5,6 +5,7 @@ import React from "react";
 
 import { usePathname } from "next/navigation";
 import { useGlobalContext } from "@/context";
+import clsx from "clsx";
 
 type NavLinkProps = {
   children: React.ReactNode;
@@ -28,12 +29,29 @@ export default function NavLink({
       : "";
 
   return (
-    <Link
-      href={href}
-      className={`${activeClass} ${className} group flex items-center gap-2 w-fit h-fit my-[2px] p-2 py-1 md:p-0 md:m-0 hover:font-semibold transition-all duration-300`}
-      onClick={() => setIsMenuClicked(false)}
+    <div
+      className={clsx(
+        "group/navLink w-fit h-fit my-[2px] p-2 py-1 md:text-sm md:p-0 md:m-0",
+        activeClass,
+        className
+      )}
     >
-      {children}
-    </Link>
+      <Link
+        href={href}
+        className={`flex items-center gap-2  hover:font-semibold transition-all duration-500`}
+        onClick={() => setIsMenuClicked(false)}
+      >
+        {children}
+      </Link>
+      <div
+        className={clsx(
+          "group-hover/navLink:w-3/4 group-hover/navLink:h-0.5 bg-primary transition-all duration-500",
+          {
+            "w-3/4 h-0.5": pathname === href,
+            "w-0": pathname !== href,
+          }
+        )}
+      />
+    </div>
   );
 }
