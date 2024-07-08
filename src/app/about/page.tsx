8d94eb8c64ea6c94ca962e22e53424b786ff4806
nathaniyelll <essien.nathaniyelll@gmail.com>
2024-07-08 22:19:47 +0100

    update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -77,32 +77,37 @@ const page = () => {
 
             <h1 className="font-bold text-2xl text-blue-950">History</h1>
           </div>
-          <p className="text-zinc-700">
-            The Department of Chemistry started as the Department of Pure and
+          <div className="text-zinc-700 flex flex-col gap-3">
+            <p>The Department of Chemistry started as the Department of Pure and
             Applied Chemistry in 1983 in the then University of Cross River State,
             Uyo. It grew up into the Department of Chemistry and Biochemistry. In
             2001, Biochemistry programme was demerged from the department and
             elevated to a substantive department, which formed the nucleus of the
-            faculty of Basic Medical Sciences of the University of Uyo. Hitherto,
-            the Department had been offering courses leading to B.Sc. degree in Pure
+            faculty of Basic Medical Sciences of the University of Uyo.</p>
+            
+            <p>Hitherto,the Department had been offering courses leading to B.Sc. degree in Pure
             and Applied Chemistry until Senate of the University of Uyo abolished
-            the dual programmes in 2002.The department now teaches courses leading
-            to the award of the Bachelor (B.Sc) of Science/Postgraduate (MSc. and
-            Ph.D) degree in Chemistry. On August 14, 2013 the Department re-located
+            the dual programmes in 2002.<br /> The department now teaches courses leading
+            to the award of the Bachelor of Science  (B.Sc) / Postgraduate (MSc. and
+            Ph.D) degree in Chemistry. </p>
+            
+           <p> On August 14, 2013 the Department re-located
             from the Town Campus of the University to her present location in the
             Faculty of Science Building, Permanent campus, Use Offot.Currently the
             department has six (6) units, namely; Analytical Chemistry,
             Environmental Chemistry, Industrial/Polymer Chemistry, Inorganic
-            Chemistry, Organic Chemistry and Physical Chemistry units.As a central
+            Chemistry, Organic Chemistry and Physical Chemistry units.
+            </p>
+           <p> As a central
             course to all other science disciplines and technology, the degree
             programme in Chemistry is designed to foster in undergraduates and
             postgraduates an appreciation of this centrality of chemistry to human
             well-being, as well as its linkage to, and interactions with other
-            science disciplines and the industry.Our degree programme also
+            science disciplines and the industry. <br />Our degree programme also
             stimulates the development of a broad range of skills applied in
             chemical and non-chemical areas, and this provides confidence for
-            employment and entrepreneurship.
-          </p>
+            employment and entrepreneurship.</p>
+          </div>
         </section>
         <div id="philosophy" className="mt-6 md:mt-4 md:p-2 md:w-10/12 mx-auto">
           <div className="flex items-center gap-2 md:text-xl mb-2">
