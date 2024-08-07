5c8d43069ca595c252837c8baa992faff63204b3
Nathaniyell <essien.nathan@yahoo.com>
2024-08-07 13:13:30 +0100

    update lecturersData.ts

--- src/lib/lecturersData.ts
+++ src/lib/lecturersData.ts
@@ -277,7 +277,7 @@ const staffData: Lecturer[] = [
     profile: "",
     experience: [
       {
-        title: "Professor of Chemistry, Department of Chemistry, University of Uyo",
+        title: "Professor of Oorganic Chemistry, Department of Chemistry, University of Uyo",
         year: "2014",
       },
       {
