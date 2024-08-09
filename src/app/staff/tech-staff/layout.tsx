5fa461c077443420d08a2c22f07c13af298a3795
Nathaniyell <essien.nathan@yahoo.com>
2024-08-09 12:10:31 +0100

    update metadata for non-teaching staff page

---
+++ src/app/staff/tech-staff/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {   
+         title: 'Non-teaching staff',
+     
+
+  description: "Meet our dedicated non-teaching staff who support the daily operations and enhance the learning environment of our department. From laboratory, administration to maintenance, our team is committed to ensuring a smooth and productive experience for staff and student of the department of chemistry university of uyo",
+  keywords: [
+    "Non-teaching staff",
+    "University of Uyo",
+    "Department of Chemistry",
+    "Support staff",
+    "Administrative staff",
+    "Laboratory staff",
+  ]
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <body>
+   
+          
+
+          {children}
+ 
+          </body>
+      
+    </>
+  );
+}
