005e4e17344e65cdde0ef7d1fbac2d3d882b31c2
Nathaniyell <essien.nathan@yahoo.com>
2025-03-16 22:13:36 +0100

    remove comments from footer

--- src/lib/footerData.ts
+++ src/lib/footerData.ts
@@ -1,23 +1,3 @@
-// export const resourceQuickLinks = [
-//   { href: "resources#handbook", title: "handbook" },
-//   { href: "resources#constitution", title: "constitution" },
-//   { href: "resources#magazines", title: "magazines" },
-//   { href: "resources#faqs", title: "faqs" },
-// ];
-
-// export const researchQuickLinks = [
-//   { href: "/programmes/undergraduate", title: "undergraduate" },
-//   { href: "/programmes/postgraduate", title: "postgraduate" },
-//   { href: "/research", title: "research" },
-//   { href: "/resources", title: "resources" },
-// ];
-
-// export const deptQuickLinks = [
-//   { href: "/about", title: "about" },
-//   { href: "/recent-news", title: "news" },
-//   { href: "/staff/teach-staff", title: "teaching staff" },
-//   { href: "/staff/tech-staff", title: "technical staff" },
-// ];
 
 export const resourceQuickLinks = [
   { href: "resources#handbook", title: "handbook" },
